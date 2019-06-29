2a8bd43044b30892dd6dca6ce454b827248e4ae1
sha <1373811094@qq.com>
2019-06-29 20:07:19 +0800

    生产线controller、mapper、jsp

---
+++ wms-web/src/main/java/com/wms/controller/ProductLineController.java
@@ -0,0 +1,102 @@
+package com.wms.controller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+
+import com.wms.mybatis.dao.ProductLineMybatisDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import com.wms.mybatis.entity.SfcUserLogin;
+import com.wms.service.ProductLineService;
+import com.wms.utils.ResourceUtil;
+import com.wms.utils.annotation.Login;
+import com.wms.vo.Json;
+import com.wms.vo.ProductLineVO;
+import com.wms.easyui.EasyuiCombobox;
+import com.wms.easyui.EasyuiDatagrid;
+import com.wms.easyui.EasyuiDatagridPager;
+import com.wms.vo.form.ProductLineForm;
+import com.wms.query.ProductLineQuery;
+
+@Controller
+@RequestMapping("productLineController")
+public class ProductLineController {
+
+	@Autowired
+	private ProductLineService productLineService;
+
+
+
+	@Login
+	@RequestMapping(params = "toMain")
+	public ModelAndView toMain(String menuId) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("menuId", menuId);
+		return new ModelAndView("productLine/main", model);
+	}
+
+	@Login
+	@RequestMapping(params = "showDatagrid")
+	@ResponseBody
+	public EasyuiDatagrid<ProductLineVO> showDatagrid(EasyuiDatagridPager pager, ProductLineQuery query) throws Exception {
+		EasyuiDatagrid<ProductLineVO> pagedDatagrid = productLineService.getPagedDatagrid(pager, query);
+		return pagedDatagrid;
+	}
+
+
+	@Login
+	@RequestMapping(params = "add")
+	@ResponseBody
+	public Json add(ProductLineForm productLineForm) throws Exception {
+		Json json = productLineService.addProductLine(productLineForm);
+		if(json == null){
+			json = new Json();
+			json.setMsg(ResourceUtil.getProcessResultMsg(json.isSuccess()));
+		}
+		return json;
+	}
+
+	@Login
+	@RequestMapping(params = "edit")
+	@ResponseBody
+	public Json edit(ProductLineForm productLineForm) throws Exception {
+		Json json = productLineService.updateProductLine(productLineForm);
+		if(json == null){
+			json = new Json();
+			json.setMsg(ResourceUtil.getProcessResultMsg(json.isSuccess()));
+		}
+		return json;
+	}
+
+	@Login
+	@RequestMapping(params = "delete")
+	@ResponseBody
+	public Json delete(String id) {
+		Json json = productLineService.deleteProductLine(id);
+		if(json == null){
+			json = new Json();
+			json.setMsg(ResourceUtil.getProcessResultMsg(json.isSuccess()));
+		}
+		return json;
+	}
+
+	@Login
+	@RequestMapping(params = "getBtn")
+	@ResponseBody
+	public Json getBtn(String id, HttpSession session) {
+		return productLineService.getBtn(id, (SfcUserLogin)session.getAttribute(ResourceUtil.getUserInfo()));
+	}
+
+	@Login
+	@RequestMapping(params = "getCombobox")
+	@ResponseBody
+	public List<EasyuiCombobox> getCombobox() {
+		return productLineService.getProductLineCombobox();
+	}
+
+}
