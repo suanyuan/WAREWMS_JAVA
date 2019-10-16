ec5ed36a7175d26163046d3637c1e37df82b7d01
haki <haki123456>
2019-10-16 16:00:35 +0800

    “发运订单： 订单发运增加弹窗事件 选择是否需要签回单证正在完善中.... 待完善

--- wms-service/src/main/java/com/wms/service/sfExpress/RequestXmlUtil.java
+++ wms-service/src/main/java/com/wms/service/sfExpress/RequestXmlUtil.java
@@ -1,5 +1,6 @@
 package com.wms.service.sfExpress;
-import	java.text.SimpleDateFormat;
+
+import java.text.SimpleDateFormat;
 
 
 import com.wms.entity.order.OrderHeaderForNormal;
@@ -7,8 +8,8 @@ import com.wms.vo.form.OrderHeaderForNormalForm;
 
 /**
  * 顺丰请求的工具类
- * @author 李宇
  *
+ * @author 李宇
  */
 public class RequestXmlUtil {
 
@@ -18,13 +19,10 @@ public class RequestXmlUtil {
     public static final String CLIENT_CODE = "HJXXJS";
 
 
-
-
-   /**
-    * 获取下单请求体
-   */
-public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderForNormal) {
-
+    /**
+     * 获取下单请求体
+     */
+    public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderForNormal, String returnSfOrder) {
 
 
         StringBuilder strBuilder = new StringBuilder();
@@ -35,9 +33,9 @@ public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderF
         strBuilder.append("<Order").append(" ");
         //客户订单号,建议英文字母+
         //YYMMDD(日期)+流水号,
-        //如:TB1207300000001
-        strBuilder.append("orderid='" +orderHeaderForNormal.getOrderno()+"22"+ "'").append(" ");
-        //返回顺丰运单号
+        //如:TB1207300000001  TODO 测试使用流水号
+        strBuilder.append("orderid='" + orderHeaderForNormal.getOrderno() + "22" + "'").append(" ");
+        //返回顺丰运单号  //运输方式
         strBuilder.append("express_type='1'").append(" ");
         //寄件方信息
         strBuilder.append("j_province='" + "江苏省" + "'").append(" ");
@@ -56,13 +54,13 @@ public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderF
 //        strBuilder.append("d_contact='" + "高俊" + "'").append(" ");//到件方联系人
 //        strBuilder.append("d_address='" + "白云东路130号" + "'").append(" ");//到件方详细地址
 
-    strBuilder.append("d_province='" + orderHeaderForNormal.getCProvince() + "'").append(" ");//省
-    strBuilder.append("d_city='" +  orderHeaderForNormal.getCCity() + "'").append(" ");//市
-    strBuilder.append("d_county='" + orderHeaderForNormal.getCAddress2()  + "'").append(" ");//区
-    strBuilder.append("d_company='" + orderHeaderForNormal.getConsigneeid() + "'").append(" ");//到件方公司名称
-    strBuilder.append("d_tel='" +  orderHeaderForNormal.getCTel1() + "'").append(" ");//到件方联系电话
-    strBuilder.append("d_contact='" + orderHeaderForNormal.getCContact()  + "'").append(" ");//到件方联系人
-    strBuilder.append("d_address='" + orderHeaderForNormal.getCAddress1() + "'").append(" ");//到件方详细地址
+        strBuilder.append("d_province='" + orderHeaderForNormal.getCProvince() + "'").append(" ");//省
+        strBuilder.append("d_city='" + orderHeaderForNormal.getCCity() + "'").append(" ");//市
+        strBuilder.append("d_county='" + orderHeaderForNormal.getCAddress2() + "'").append(" ");//区
+        strBuilder.append("d_company='" + orderHeaderForNormal.getConsigneeid() + "'").append(" ");//到件方公司名称
+        strBuilder.append("d_tel='" + orderHeaderForNormal.getCTel1() + "'").append(" ");//到件方联系电话
+        strBuilder.append("d_contact='" + orderHeaderForNormal.getCContact() + "'").append(" ");//到件方联系人
+        strBuilder.append("d_address='" + orderHeaderForNormal.getCAddress1() + "'").append(" ");//到件方详细地址
         //备注不为空就进行xml append()拼接
        /* try {
             if(!expressOrder.getRemark().equals(" ") || expressOrder != null){
@@ -72,10 +70,14 @@ public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderF
             e.printStackTrace();
         }*/
 
-            //温度范围类型 1为冷藏   3为冷冻
+        //温度范围类型 1为冷藏   3为冷冻
         //strBuilder.append("temp_range='1'").append(" ");
-            //是否要求签回单号
-        strBuilder.append("need_return_tracking_no='1'").append(" ");
+        //是否要求签回单号  根据前端选中是否
+        if (returnSfOrder.equals("1")) {
+            strBuilder.append("need_return_tracking_no='1'").append(" ");
+            strBuilder.append("routelabelService='1'").append(" ");
+        }
+
         //货物信息
         strBuilder.append("parcel_quantity='1'").append(" ");
         //货物-总-重量
@@ -84,11 +86,11 @@ public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderF
         strBuilder.append("custid ='" + CallExpressServiceTools.CUST_ID + "'").append(" ");//顺丰月结卡号
 
         //付款方式:1寄付 2.收支付 3.第三方付
-        strBuilder.append("pay_method = '"+"1"+"'").append(" ");
+        strBuilder.append("pay_method = '" + "1" + "'").append(" ");
         strBuilder.append(" is_unified_waybill_no='1'>").append(" ");
 
-    //海关批次（默认空的）
-       // strBuilder.append("customs_batchs=''").append(" ");
+        //海关批次（默认空的）
+        // strBuilder.append("customs_batchs=''").append(" ");
         //货物类型
         strBuilder.append("<Cargo").append(" ");
         strBuilder.append("name='医药器械'>").append(" ");
@@ -100,9 +102,9 @@ public static String getOrderServiceRequestXml(OrderHeaderForNormal orderHeaderF
     }
 
 
-
     /**
      * 取消顺丰订单请求体
+     *
      * @param orderNo 订单号
      * @return
      */
