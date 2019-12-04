76044144dee11ed93b5f6d60626963a1005f4dd6
jwk <1179916068@qq.com>
2019-12-04 12:02:25 +0800

    入库单列表修改

--- wms-entity/src/main/java/com/wms/entity/RptAsnList.java
+++ wms-entity/src/main/java/com/wms/entity/RptAsnList.java
@@ -18,20 +18,23 @@ public class RptAsnList implements Serializable {
     private String asndate;            //日期
     private String lotatt03StartDate;            //开始日期
     private String lotatt03EndDate;            //结束日期
-    private String lotatt05;            //序列号
     private String documentType;         //单据类型
     private String warehouse;            //仓库
     private String customerid;           //供货单位
     private String sku;                  //存货编码
     private String lotatt12;             //存货名称
     private String descrc;              //规格型号
+    private String reservedfield02;               //产品描述
     private Double asnqtyeach;          //入库数量
     private Double asnqty;               //入库件数
     private Double soqtyeach;           //出库数量
     private Double soqty;               //出库件数
     private String uom;               //主计量单位
     private String lotatt04;               //批号
-    private String reservedfield02;               //产品描述
+    private String lotatt05;            //序列号
+    private String lotatt06;            //注册证号
+    private String lotatt01;            //生产日期
+    private String lotatt02;            //效期
     private String purchaseOrderNumber;   //采购订单号
     private String notes;             //备注
     private Double planPrice;          //计划价
