1ac0cd860223cbd22cfb5a73abcd712b11e163b9
jwk <1179916068@qq.com>
2020-01-10 11:10:15 +0800

    药监核查-委托客户-主页营业执照编号修改为引用企业信息-营业执照-统一社会信用代码

--- wms-entity/src/main/java/com/wms/entity/SearchBasCustomer.java
+++ wms-entity/src/main/java/com/wms/entity/SearchBasCustomer.java
@@ -16,6 +16,7 @@ public class SearchBasCustomer implements Serializable {
 
 	private String enterpriseName;  //委托方企业名称
 	private String licenseNumber;  //营业执照编号
+	private String socialCreditCode;  //统一社会信用代码
 	private String residence;  //住所
 	private String juridicalPerson;  //法定代表人
 	private String headName;  //企业负责人
